9e6f968a8558723df9ca8c73b5600c820222fcaf
su100 <zxctys@naver.com>
2020-08-05 21:28:20 +0900

    Updates

---
+++ precache-manifest.84f5ff3af8bc20acb93b04cfdb856084.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ad7a2ef8069ef0da92977205ca8793",
+    "url": "/dalgona-vote-front/index.html"
+  },
+  {
+    "revision": "342eaccf91d4fffd14ce",
+    "url": "/dalgona-vote-front/static/css/main.4a0f9b9a.chunk.css"
+  },
+  {
+    "revision": "968ebbffedb0a303b063",
+    "url": "/dalgona-vote-front/static/js/2.38af322a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dalgona-vote-front/static/js/2.38af322a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "342eaccf91d4fffd14ce",
+    "url": "/dalgona-vote-front/static/js/main.ae58a33b.chunk.js"
+  },
+  {
+    "revision": "ea1d26b52e3519044523",
+    "url": "/dalgona-vote-front/static/js/runtime-main.ae256033.js"
+  },
+  {
+    "revision": "dfb2ed3d831e71e6206f19b4efb13132",
+    "url": "/dalgona-vote-front/static/media/blackpink.dfb2ed3d.png"
+  },
+  {
+    "revision": "7588aad38547a8c6241c5698c83affce",
+    "url": "/dalgona-vote-front/static/media/crown.7588aad3.png"
+  },
+  {
+    "revision": "37a8df571d3c736bd63da7e19cbb389d",
+    "url": "/dalgona-vote-front/static/media/moon.37a8df57.png"
+  },
+  {
+    "revision": "7a527e027df25b91be4c9d50ff92fe7f",
+    "url": "/dalgona-vote-front/static/media/ssak.7a527e02.png"
+  }
+]);
