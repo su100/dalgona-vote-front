6e565cb04a673da8345201338969b10fedf3bab7
su100 <zxctys@naver.com>
2020-08-06 16:12:37 +0900

    apply page template to NotFoundPage

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,7 +1,21 @@
 import React from "react";
+import PageTemplate from "components/common/PageTemplate";
+import Header from "components/common/Header";
 
-const NotFoundPage = () => {
-    return <div>NotFound</div>;
+const NotFoundPage = (props) => {
+    return (
+        <PageTemplate
+            header={
+                <Header
+                    location={props.location}
+                    history={props.history}
+                    match={props.match}
+                />
+            }
+        >
+            NOT FOUND
+        </PageTemplate>
+    );
 };
 
 export default NotFoundPage;
