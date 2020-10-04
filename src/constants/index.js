137b70b32d06f311caeccc4b20d01515447eece6
su100 <zxctys@naver.com>
2020-10-04 21:13:13 +0900

    fix typo

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 let url;
 
 //개발 운영 api 주소 분기
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV === "production") {
   url = "http://15.164.97.53";
 } else {
   url = "http://localhost:8000";
